54347e4e4032585ec5bf4849684cea8e6be840d6
Kevin <kevincarr632@gmail.com>
2020-07-02 15:46:34 -0500

    completed ring_buffer.py

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,10 +1,22 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.head = 0
+        self.buffer = []
 
     def append(self, item):
-        # When the ring buffer is full and a new element is inserted, the oldest element in the ring buffer is overwritten with the newest element.
-        pass
+        # check if the buffer is currently full
+        if len(self.buffer) == self.capacity:
+            # overwright the current head of buffer
+            self.buffer[self.head] = item
+            '''move the head to the next item
+                each time we append
+                loop back to the begining when we reach the end'''
+            self.head = (self.head + 1) % self.capacity
+        else:
+            # if not full just add item to the end of buffer
+            self.buffer.append(item)
+            
 
     def get(self):
-        pass
+        return self.buffer
